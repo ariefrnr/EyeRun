8124f25ebb76e4ef86e1c42cfcda3cb58b43ab7b
ariefrnr <arief.roihan.nur@gmail.com>
2025-03-21 14:18:23 +0700

    Initial Commit

---
+++ EyeRun/EyeRunApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeRunApp.swift
+//  EyeRun
+//
+//  Created by Arief Roihan Nur Rahman on 21/03/25.
+//
+
+import SwiftUI
+
+@main
+struct EyeRunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
