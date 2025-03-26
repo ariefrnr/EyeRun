51a7de7305237aaa86a4ba201c92b13c3d7dcec1
ariefrnr <arief.roihan.nur@gmail.com>
2025-03-26 08:07:44 +0700

    Adding Layout
    Add layout for put component in BaselineTracker page

---
+++ EyeRun/Views/BaselineTracker/DateView.swift
@@ -0,0 +1,22 @@
+//
+//  DateView.swift
+//  EyeRun
+//
+//  Created by Arief Roihan Nur Rahman on 26/03/25.
+//
+
+import SwiftUI
+
+struct DateView: View {
+    var body: some View {
+        VStack {
+            Color.blue
+            Color.yellow
+        }
+    }
+}
+
+
+#Preview {
+    DateView()
+}
